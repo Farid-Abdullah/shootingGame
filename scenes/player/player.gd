8971f39164db25b0316378244b3736800aed4194
Farid <60304642@udst.edu.qa>
2025-05-24 22:37:21 +0300

    player follows mouse now

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,11 +1,15 @@
 extends Node2D
-
+@export var rotation_speed:=100.0
+func _physics_process(delta):
+	$playerImage.look_at(get_global_mouse_position())
+	
 func _ready():
 	pass
 func _process(delta):
-	pass
+	
 	var direction = Input.get_vector("left","right","up","down")
 	position+= 800*delta*direction
-	
 	if Input.is_action_just_pressed("secondary action"):
-		print("grenade!")
+		print("GRENADE!! (right click or space)")
+	if Input.is_action_just_pressed("primary action"):
+		print("SHOOTING LASER! (left click)")
